74976a84e41d9a820d3637b5c0f6b82a5ada9d91
EtheaaryWasTaken <58019814+EtheaaryWasTaken@users.noreply.github.com>
2022-03-27 21:47:39 +1300

    Create trashyicons.js

---
+++ trashyicons.js
@@ -0,0 +1,15 @@
+/*
+  THIS ICON PACK REQUIRES jQUERY. IT WILL NOT WORK WITHOUT IT.
+*/
+
+window.addEventListener('load', function() {
+    if (window.jQuery) {  
+        // jQuery is loaded  
+    } else {
+        // jQuery is not loaded
+        console.log("")
+    }
+
+    $(".ti-exclamation").html("<svg xmlns="http://www.w3.org/2000/svg" id="Layer_2" data-name="Layer 2" viewBox="0 0 1.88 11.3"><defs><style>.cls-1{fill:none;stroke:#231f20;stroke-miterlimit:10;}</style></defs><rect class="cls-1" x="0.5" y="0.5" width="0.88" height="7.99" rx="0.44"/><path class="cls-1" d="M.94,9.91a.44.44,0,0,1,.44.44h0a.45.45,0,0,1-.44.45h0a.45.45,0,0,1-.44-.45h0a.44.44,0,0,1,.44-.44"/></svg>");
+
+})
